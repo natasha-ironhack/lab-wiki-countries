7410fbfb6b1f402baf6205b22f7af31518d05ba4
Natasha Smith <pisceanchic@gmail.com>
2021-10-16 13:04:59 +0200

    Iteration 1 & 2 done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 //import Home from './components/Home';
+import countries from './countries.json';
 import Navbar from './components/Navbar';
 import CountriesList from './components/CountriesList';
 import CountryDetails from './components/CountryDetails';
@@ -10,10 +11,14 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <CountriesList />
-      <Switch>
-        <Route path="/country/:cca3" component={CountryDetails} />
-      </Switch>
+      <div className="container">
+        <div className="row">
+          <CountriesList countries={countries} />
+          <Switch>
+            <Route path="/country/:cca3" component={CountryDetails} />
+          </Switch>
+        </div>
+      </div>
     </div>
   );
 }
