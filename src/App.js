138cd24705ec46f1422595c24ec3284be0c12356
Natasha Smith <pisceanchic@gmail.com>
2021-10-19 17:01:13 +0200

    Iteration 3 half done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-//import logo from './logo.svg';
+import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 //import Home from './components/Home';
@@ -7,20 +7,30 @@ import Navbar from './components/Navbar';
 import CountriesList from './components/CountriesList';
 import CountryDetails from './components/CountryDetails';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <div className="container">
-        <div className="row">
-          <CountriesList countries={countries} />
-          <Switch>
-            <Route path="/country/:cca3" component={CountryDetails} />
-          </Switch>
+//Iteration 3 | Set the state when the component mounts
+// Our App.js application should have its own state
+// holding one property,countries, holding the data coming
+// from the countries.json file. The state should be set
+// only once the component is rendered to the DOM, using
+// the lifecycle method componentDidMount() .
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <div className="container">
+          <h1>Countries</h1>
+          <div className="row">
+            <CountriesList countries={countries} />
+            <Switch>
+              <Route path="/country/:cca3" component={CountryDetails} />
+            </Switch>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
