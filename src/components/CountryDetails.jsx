d3c766c94480cf9300dd28294d467f3504846d13
Natasha Smith <pisceanchic@gmail.com>
2021-10-19 17:10:38 +0200

    Iteration 3 partially done

--- src/components/CountryDetails.jsx
+++ src/components/CountryDetails.jsx
@@ -4,6 +4,33 @@ import countries from '../countries.json';
 import { Link } from 'react-router-dom';
 
 export default class CountryDetails extends Component {
+  state = {
+    countries: countries,
+    country: null,
+  };
+
+  getInfo() {
+    const country = this.state.countries.filter((eachCountry) => {
+      return eachCountry.cca3 === this.props.match.params.cca3;
+    })[0];
+
+    this.setState({ country: country });
+  }
+
+  componentDidMount() {
+    this.getInfo();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (
+      prevState.country &&
+      this.props.match.params.cca3 !== prevProps.match.params.cca3
+    ) {
+      this.getInfo();
+    }
+    console.log(prevState);
+  }
+
   render() {
     return <div>{this.props.match.params.cca3}</div>;
   }
